99ce9b3d8150922bba91b4adbbaf7938f1b6ac1e
Julian Wilson <julianrwilson@gmail.com>
2014-06-17 16:01:10 -0700

    Updated Database Config

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'forge',
 			'username'  => 'forge',
-			'password'  => '',
+			'password'  => 'XTGsYESZx9BLsO0um1ug',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
