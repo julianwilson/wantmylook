4cbcbf69afd531f636196b7b7bde7f85f7418adb
Julian Wilson <julianrwilson@gmail.com>
2014-06-18 13:10:54 -0700

    removed oauth code

--- app/routes.php
+++ app/routes.php
@@ -11,44 +11,9 @@
 |
 */
 
-function loginWithInstagram() {
 
-    // get data from input
-    $code = Input::get( 'code' );
-
-    // get ig service
-    $ig = OAuth::consumer( 'Instagram' );
-
-    // check if code is valid
-
-    // if code is provided get user data and sign in
-    if ( !empty( $code ) ) {
-
-        // This was a callback request from instagram, get the token
-        $token = $ig->requestAccessToken( $code );
-
-        // Send a request with it
-        $result = json_decode( $ig->request( '/users/self/media/liked' ), true );
-
-        //Var_dump
-        //display whole array().
-        dd($result);
-
-    }
-    // if not ask for permission first
-    else {
-        // get ig authorization
-        $url = $ig->getAuthorizationUri();
-
-        // return to instagram login url
-         return Redirect::to( (string)$url );
-    }
-
-}
 
 Route::get('/', function()
 {
 	return View::make('hello');
 });
-
-Route::get('/authenticate', loginWithInstagram);
