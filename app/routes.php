4fe81807419b80c9cd76d8906fc75766f78a0c75
Julian Wilson <julianrwilson@gmail.com>
2014-06-18 11:12:42 -0700

    Added oAuth Laravel package and updated routes accordingly

--- app/routes.php
+++ app/routes.php
@@ -15,22 +15,3 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
-
-Route::get('/authenticate', function()
-{
-	$instaConfig = array(
-        'apiKey' => 'e3f5ca5f0bb44527bf4d07b8af9b4e90',
-        'apiSecret' => '0b2710a239f140248262c63b0260bff6',
-        'apiCallback' => 'http://54.183.26.46/instaamp/authenticate',
-     );
-	 
-	// Get the code and pass it to our handshake script
-	$code = Input::get('code');
-	
-	session_start();
-	// Instantiate the API handler object
-	$instagram = new Instagram($instaConfig);
-	$accessToken = $instagram->getOAuthToken($code);
-	$_SESSION['InstagramAccessToken'] = $accessToken;
-	return $accessToken;
-});
