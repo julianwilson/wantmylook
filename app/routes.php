d525a195f6fd4424b87b6ce79c8ff3a241ffe8a2
Julian Wilson <julianrwilson@gmail.com>
2014-06-18 14:55:46 -0700

    grr routes

--- app/routes.php
+++ app/routes.php
@@ -11,11 +11,6 @@
 |
 */
 
-function loginWithInstagram() {
-
-    return 'yes';
-}
-
 Route::get('/', function()
 {
 	return View::make('hello');
@@ -23,5 +18,34 @@ Route::get('/', function()
 
 Route::get('/authenticate', function()
 {
-	return 'yes';
+	// get data from input
+    $code = Input::get( 'code' );
+
+    // get ig service
+    $ig = OAuth::consumer( 'Instagram' );
+
+    // check if code is valid
+
+    // if code is provided get user data and sign in
+    if ( !empty( $code ) ) {
+
+        // This was a callback request from instagram, get the token
+        $token = $ig->requestAccessToken( $code );
+
+        // Send a request with it
+        $result = json_decode( $ig->request( '/users/self/media/liked' ), true );
+
+        //Var_dump
+        //display whole array().
+        dd($result);
+
+    }
+    // if not ask for permission first
+    else {
+        // get ig authorization
+        $url = $ig->getAuthorizationUri();
+
+        // return to instagram login url
+         return Redirect::to( (string)$url );
+    }
 });
